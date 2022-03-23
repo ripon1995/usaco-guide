e6d7254df09549d6426a1d60145e5624db393270
MD AL-AMIN RIPON <mdalaminripondc1995@gmail.com>
2022-03-24 05:25:53 +0600

    Till input output

--- general/test.cpp
+++ general/test.cpp
@@ -2,6 +2,19 @@
 using namespace std;
 
 int main() {
-    cout << "Hello For War"<< endl;
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int num;
+    cin>>num;
+    cout << num <<" ";
+    while(num>1) {
+        if(num%2 == 0) {
+            num/=2;
+        }
+        else {
+            num = (3*num)+1;
+        }
+        cout << num <<" ";
+    }
     return 0;
 }
