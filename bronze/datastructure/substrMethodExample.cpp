c1e328aad02d1f42b91d0c8c73710e9614a56cbd
MD AL-AMIN RIPON <mdalaminripondc1995@gmail.com>
2022-03-31 03:19:37 +0600

    bronze-simulation ====> practice cf

---
+++ bronze/datastructure/substrMethodExample.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+string addSpacesBetweenEachWord(string s,vector<int>& spaces) {
+    int pos = 0;
+    string res ="";
+    for(int i=0;i<spaces.size();i++) {
+        res += s.substr(pos,spaces[i]-pos);
+        res += " ";
+        pos = spaces[i];
+    }
+    res += s.substr(pos);
+    return res;
+}
+
+int main()
+{
+    vector<int> spaces = {8,13,15};
+    string str = "leetcodehelpsmelearn";
+    cout << addSpacesBetweenEachWord(str,spaces) << endl;
+    return 0;
+}
